d9b0f9306191cdb2aa1f63a987d0902d5fc4bb3d
Ricardo <danmleal@gmail.com>
2017-11-07 12:37:41 -0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,17 +15,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome',
+        createdAt: new Date().getTime()
     });
 
-    socket.on('createMessage', (message) => {
-        console.log('Create Message', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.on('createMessage', (message) => {
+    //     console.log('Create Message', message);
+    //     socket.broadcast.emit('newMessage', {
+    //         from: message.from,
+    //         text: message.text,
+    //         createdAt: new Date().getTime()
+    //     });
+    // });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
