36737ecfe46e4d5b77f1900de73995daad9dc284
Ricardo <danmleal@gmail.com>
2017-11-07 12:02:59 -0600

    Emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Dan',
-        text: 'Hi'
-    });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,18 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: 'Server',
-        text: 'Welcome to chat room',
-        createAt: 123
-    });
-
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 
     socket.on('createMessage', (message) => {
         console.log('Create Message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 });
 
